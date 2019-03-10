e4d0c66a65e59442611617a81abcedb2d20fa3e0
NavyaZaveri <navyajayesh+1@gmail.com>
2019-03-10 17:48:51 +0000

    put pretty printer in a separate file

---
+++ src/main/kotlin/interpreter/PrettyPrinter.kt
@@ -0,0 +1,39 @@
+package interpreter
+
+import parser.BinaryExpr
+import parser.Expr
+import parser.LiteralExpr
+import parser.UnaryExpr
+
+
+abstract class PrettyPrinter : Visitor<String> {
+    fun prettyPrint(expr: Expr): String {
+        return expr.accept(this)
+    }
+
+    abstract fun wrap(operand: String, vararg exprs: Expr): String
+
+    override fun visitBinaryExpression(expr: BinaryExpr): String {
+        return wrap(expr.operand.lexeme, expr.left, expr.right)
+    }
+
+    override fun visitLiteralExpression(expr: LiteralExpr): String {
+        return expr.token.literal.toString()
+    }
+
+    override fun visitUnaryExpression(expr: UnaryExpr): String {
+        return wrap(expr.token.lexeme, expr.left)
+    }
+}
+
+
+//A reversed polish notation based pretty printer
+class Rpn : PrettyPrinter() {
+    override fun wrap(operand: String, vararg exprs: Expr): String {
+        var res = ""
+        for (expr in exprs) {
+            res += " " + prettyPrint(expr)
+        }
+        return "$res $operand".trim()
+    }
+}
